d3d16c2cf3648cd10d3fc2e3ba341f2e476852bf
Fillipe de Menezes <fmcs3@cin.ufpe.br>
2015-06-23 16:14:31 -0300

    # Bug de cookies incorretos corrigido #

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,13 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    def raise_and_rescue
+      begin
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      rescue
+        session[:user_id] = nil
+      end
+    end
   end
 
   def require_user
