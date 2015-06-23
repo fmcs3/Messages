d5f2c7c15df2bbbf2a2e49bc5b6af7bf3976586d
Fillipe de Menezes <fmcs3@cin.ufpe.br>
2015-06-23 16:12:38 -0300

    SqlLite to Postgres #2

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150620165833) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.string   "title"
     t.string   "message"
